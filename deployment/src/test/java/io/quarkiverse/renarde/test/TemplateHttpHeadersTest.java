fee892a95f6df0d5e6a2f7319015ed71a49212ec
gbourant <root@gbourant.com>
2024-02-17 16:55:34 +0200

    format

--- deployment/src/test/java/io/quarkiverse/renarde/test/TemplateHttpHeadersTest.java
+++ deployment/src/test/java/io/quarkiverse/renarde/test/TemplateHttpHeadersTest.java
@@ -1,10 +1,9 @@
 package io.quarkiverse.renarde.test;
 
-import io.quarkiverse.renarde.Controller;
-import io.quarkus.qute.CheckedTemplate;
-import io.quarkus.qute.TemplateInstance;
-import io.quarkus.test.QuarkusUnitTest;
+import static io.restassured.RestAssured.when;
+
 import jakarta.ws.rs.Path;
+
 import org.hamcrest.Matchers;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
@@ -12,21 +11,23 @@ import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-import static io.restassured.RestAssured.when;
+import io.quarkiverse.renarde.Controller;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+import io.quarkus.test.QuarkusUnitTest;
 
 public class TemplateHttpHeadersTest {
 
     @RegisterExtension
     static final QuarkusUnitTest config = new QuarkusUnitTest()
             .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
-                            .addClasses(MyController.class)
-                            .addAsResource(new StringAsset("""
-                                    template content
-                                    {#fragment id="fragment" rendered=false}
-                                    fragment template
-                                    {/fragment}
-                                    """), "templates/MyController/template.html")
-            );
+                    .addClasses(MyController.class)
+                    .addAsResource(new StringAsset("""
+                            template content
+                            {#fragment id="fragment" rendered=false}
+                            fragment template
+                            {/fragment}
+                            """), "templates/MyController/template.html"));
 
     @Test
     public void testTemplateHeaders() {
@@ -51,6 +52,7 @@ public class TemplateHttpHeadersTest {
         @CheckedTemplate
         public static class Templates {
             public static native TemplateInstance template();
+
             public static native TemplateInstance template$fragment();
         }
 
