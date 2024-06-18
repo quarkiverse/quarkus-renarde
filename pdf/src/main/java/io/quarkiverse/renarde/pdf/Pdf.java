7e8b2d22809195d755a183082ee394a5738ec702
Stéphane Épardaud <stef@epardaud.fr>
2024-06-18 10:59:35 +0200

    Experimental support for generating png from html

--- pdf/src/main/java/io/quarkiverse/renarde/pdf/Pdf.java
+++ pdf/src/main/java/io/quarkiverse/renarde/pdf/Pdf.java
@@ -5,5 +5,7 @@ import jakarta.ws.rs.core.MediaType;
 public class Pdf {
 
     public final static String APPLICATION_PDF = "application/pdf";
+    public final static String IMAGE_PNG = "image/png";
     public final static MediaType APPLICATION_PDF_TYPE = new MediaType("application", "pdf");
+    public final static MediaType IMAGE_PNG_TYPE = new MediaType("image", "png");
 }
